c66969e6de79df87f1b4d4e9c78b69b90330617c
j-felder <ajustinfelder@gmail.com>
2023-07-21 15:12:39 -0700

    code challenges complete

--- code-challenges.js
+++ code-challenges.js
@@ -21,6 +21,11 @@ const fruit4 = "kiwi"
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Pseudo code:
+// fucntion
+    // input: number
+    // below, at, or above boiling point
+// conditional statement
+
 
 const temperature1 = 42
 // Expected output: "42 is below boiling point"
@@ -31,24 +36,74 @@ const temperature2 = 350
 const temperature3 = 212
 // Expected output: "212 is at boiling point"
 
+
+// const boilingPoint = 212
+
+// const tempCheck = (num) => {
+//     if (num < boilingPoint){
+//         return `${num} is below boiling point`
+//     } else if ( num === boilingPoint){
+//         return `${num} is at boiling point`
+//     } else if (num > boilingPoint){
+//         return `${num} is above boiling point`
+//     }
+// }
+
+// console.log(tempCheck(temperature1))
+// console.log(tempCheck(temperature2))
+// console.log(tempCheck(temperature3))
+
+
+// output: 
+// 42 is below boiling point
+// 350 is above boiling point
+// 212 is at boiling point
+
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Pseudo code:
+//function
+// concat two arrays
+    //input: two arrays
+    //output/return: concatenatedArray.length   
+// 
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
 
+
+const twoBaseballArrays = () => {
+    return padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns).length
+}
+
+// console.log(twoBaseballArrays(padres1984WorldSeriesRuns, padres1998WorldSeriesRuns))
+
+//output: 9
+
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
-// Pseudo code:
-
+// Pseudo code: looked up how to reverse a string https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+//.split()
+//.reverse
+//.join()
+ // you cant .reverse a string so you have to make it into an array first, then join the array elemtents back into a string after reversing the order of the arrayed string.
 const currentCohort = "Echo 2023"
 // Expected output: "3202 ohcE"
+
+
+const reverseString = (str) => {
+    return str.split("").reverse().join("");
+}
+
+//  console.log(reverseString(currentCohort))
+//output: 3202 ohcE
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Pseudo code:
+//lastIndexOf(num)
+
 
 const numberOfConnections = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 
@@ -58,12 +113,25 @@ const givenValue1 = 42
 const givenValue2 = 10
 // Expected output: 8
 
+// console.log(numberOfConnections.lastIndexOf(givenValue1))
+
+// console.log(numberOfConnections.lastIndexOf(givenValue2))
+
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Pseudo code:
+//
+
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
+
+const sortedArray = (array) => {
+    return array.sort().reverse()
+}
+
+// console.log(sortedArray(sanDiegoSummerTemperatures))
+// console.log(sortedArray(sanDiegoWinterTemperatures))
